6f6ac698d8e0842b730513e99a11f651efec5179
Omkar Ware <123940891+ifeelomii@users.noreply.github.com>
2023-11-30 11:06:36 +0530

    Delete Database/dbFile.sql

--- Database/dbFile.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE politician;
-
-CREATE TABLE tbl_compliants(c_date, forAdmin, forGS, C_ID, u_name, c_location, c_description, u_address, u_phone, c_type, c_region, u_email);
-
-CREATE TABLE tbl_gramsevak(name, email, phone, address, region, password, status, GS_ID);
-
-CREATE TABLE tbl_admin(name,password);
