3e48405f6eaac88294365723d794ad5a4a54d780
Omkar Ware <123940891+ifeelomii@users.noreply.github.com>
2024-02-06 18:48:56 +0530

    Services interfaces and class Implementations

---
+++ IAdminService.java
@@ -0,0 +1,20 @@
+package com.demo.service;
+
+import java.util.List;
+
+import com.demo.model.Admin;
+import com.demo.model.Gramsevak;
+
+public interface IAdminService {
+	boolean validateAdminServices(Admin admin);
+	
+	List<Admin> getAllAdminsServices();
+
+	Admin getByIdServices(int id);
+
+	void addNewAdminServices(Admin admin);
+
+	void updateAdminServices(Admin admin);
+
+	void deleteAdminByIdServices(int id);
+}
